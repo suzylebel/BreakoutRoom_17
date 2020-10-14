a2d71285ce23d82a7e9b1ffde4e370bd5bbf85ae
sweetbabyjay <jayfly64@gmail.com>
2020-10-13 20:05:24 -0700

    added some js for mealsAPI'

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
+const recipeNameEl = $("#recipeName");
+const mealPicEl = $("#mealPic");
+const recipeSiteEl = $("#recipeSite")
+const videoTurtEl = $("#videoTutorial")
 
+let queryURL = "https://www.themealdb.com/api/json/v1/1/random.php"
+console.log(queryURL)
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response){
+    console.log(response)
+    recipeNameEl.text(response.meals[0].strMeal);
+    mealPicEl.html('<img width=\"200\" height=\"200\" src="' + response.meals[0].strMealThumb + '">');
+    recipeSiteEl.html('<a href="' + response.meals[0].strSource + '">Recipe Here!</a>');
+    videoTurtEl.text(response.meals[0].strYoutube);
+})
