a7e67b7ade6d9775372a38deb616e4185969606d
Joel Estrada <jre.estrada@gmail.com>
2020-10-17 11:07:02 -0700

    commit before pulling origin Develop

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
 // giphy api
-
 // event listeners for cocktail and food buttons
 $("#find-cocktail").on("click", searchProduct);
 $("#find-food").on("click", searchProduct);
@@ -39,40 +38,38 @@ function searchProduct(event) {
 // trying to use the embed_url throws a CORB error in the console -- do not use response.data[0].embed_url
 
 // night planner time slots 
-// get the current day and time from https://momentjs.com/ and display it to the user
 function nightPlannerTimeSlots() {
+    // get the current day and time from https://momentjs.com/ and display it to the user
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
-
     // for loop to create the main content - rows, columns, user input form, and the save button
     for (let i = 0; i < 9; i++) {
         // rows 
         let newRow = $("<div>");
-        newRow.addClass("container");
-
+        newRow.addClass("columns timeSlotRow");
         // hourColumn
         let hourColumn = $("<div>");
-        hourColumn.addClass("columns");
+        hourColumn.addClass("column is-1 is-offset-1 timeSlotHourColumn");
         // if statements to determine the hour column text using index i. The work-hour attribute set here is used later in the code to determine if the row is in the past, present, or future
         if (i < 6) {
             hourColumn.text(i + 6 + "PM");
-            hourColumn.attr("work-hour", i + 6);
+            hourColumn.attr("work-hour", i + 18);
         } else if (i === 6) {
             hourColumn.text("12AM");
-            hourColumn.attr("work-hour", 12);
+            hourColumn.attr("work-hour", 0);
         } else {
             hourColumn.text(i - 6 + "AM");
-            hourColumn.attr("work-hour", i + 6);
+            hourColumn.attr("work-hour", i - 6);
         }
 
         // inputColumn
         let inputColumn = $("<div>");
-        inputColumn.addClass("columns");
+        inputColumn.addClass("column is-5");
         // get the current hour in 24-hour format from https://momentjs.com/ so it can be compared with the work-hour attribute to determine whether the row is in the past, present, or future
-        let currentHour = moment().format("H");
-        let currentWorkHour = parseInt(hourColumn.attr("work-hour"));
-        if (currentWorkHour < currentHour) {
+        let current24Hour = moment().format("H");
+        let current12Hour = parseInt(hourColumn.attr("work-hour"));
+        if (current12Hour < current24Hour) {
             inputColumn.addClass("past");
-        } else if (currentWorkHour > currentHour) {
+        } else if (current12Hour > current24Hour) {
             inputColumn.addClass("future");
         } else {
             inputColumn.addClass("present");
@@ -93,21 +90,68 @@ function nightPlannerTimeSlots() {
 
         // saveButtonColumn
         let saveButton = $("<button>");
-        saveButton.addClass("columns saveBtn");
+        saveButton.addClass("column is-1 saveBtn");
         saveButton.attr("button-data-index", i);
         let saveButtonIcon = $("<i>");
         saveButtonIcon.addClass("far fa-save");
-        saveButtonIcon.attr("button-data-index", i);
+        saveButtonIcon.attr({
+            "button-data-index": i,
+            style: "font-size:20px"
+        });
         // append the icon to the button
         saveButton.append(saveButtonIcon);
         // append all of the columns to the row in order
         newRow.append(hourColumn, inputColumn, saveButton);
         // append the row to the div container for display to the user
         $("#time-slots").append(newRow);
-
-        // save button event listener. this listener is put specifically on the button element so event delegation can be used to also handle if the user clicks on the icon, which is a child of the button element
-        $("button").on("click", saveButtonHandler);
     }
+    // save button event listener. this listener is put specifically on the button element so event delegation can be used to also handle if the user clicks on the icon, which is a child of the button element
+    $("button").on("click", saveButtonHandler);
+}
+
+function notesSection() {
+    // new row
+    let notesRow = $("<div>");
+    notesRow.addClass("columns");
+    // hourColumn
+    let hourColumn = $("<div>");
+    hourColumn.addClass("column is-1 timeSlotHourColumn notesSlotRow is-vcentered");
+    hourColumn.text("Notes");
+    // inputColumn
+    let notesColumn = $("<div>");
+    notesColumn.addClass("column is-5 present");
+    // form for user input
+    let userInputForm = $("<form>");
+    let userInputText = $("<textarea>");
+    userInputText.attr({
+        type: "text",
+        rows: 10
+    });
+    userInputForm.append(userInputText);
+    notesColumn.append(userInputForm);
+    // check if there is data saved to localStorage and if so, get it and add it to the correct row
+    if (localStorage.getItem("notes") === null) {
+        userInputText.text("");
+    } else {
+        userInputText.text(localStorage.getItem("notes"));
+    }
+    // saveButtonColumn
+    let saveButton = $("<button>");
+    saveButton.addClass("column is-1 saveBtn notesSlotRow");
+    saveButton.attr("button-data-index", "notes");
+    let saveButtonIcon = $("<i>");
+    saveButtonIcon.addClass("far fa-save");
+    saveButtonIcon.attr({
+        "button-data-index": "notes",
+        style: "font-size:20px"
+    });
+    // append the icon to the button
+    saveButton.append(saveButtonIcon);
+    // append all of the columns to the row in order
+    notesRow.append(hourColumn, notesColumn, saveButton);
+    // append the row to the div container for display to the user
+    $("#notes").append(notesRow);
+    $("button").on("click", saveButtonHandler);
 }
 
 // this function handles the click event, whether it be on the icon or the blue area around the icon. this function uses an if statement to determine what the user pressed (blue area or icon) and saves the user's input to local storage using DOM traversal
@@ -125,3 +169,4 @@ function saveButtonHandler(event) {
 }
 
 nightPlannerTimeSlots();
+notesSection();
