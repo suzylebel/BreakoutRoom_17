28e4daa8cd6cf754d05e2b2a9e623f04f3a81c56
sweetbabyjay <jayfly64@gmail.com>
2020-10-20 14:57:21 -0700

    got info for food into a modal

--- JS/scriptM.js
+++ JS/scriptM.js
@@ -1,21 +1,29 @@
-$("#find-food").on("click", randomFood);
-
-function randomFood(event){
-    event.preventDefault();
-    const recipeNameEl = $("#recipeName");
-    const mealPicEl = $("#mealPic");
-    const recipeSiteEl = $("#recipeSite");
-
-    let queryURL = "https://www.themealdb.com/api/json/v1/1/random.php"
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response){
-        console.log(response)
-        recipeNameEl.html('<a href="'+ response.meals[0].strYoutube +'" target=\*"_blank\*">' + response.meals[0].strMeal + '</a>');
-        mealPicEl.html('<img width=\"200\" height=\"200\" src="' + response.meals[0].strMealThumb + '">');
-        recipeSiteEl.html('<a href="' + response.meals[0].strSource + '" target=\*"_blank\*>Recipe Here!</a>');    
+$(document).ready(function () {
+    $("#find-food").on("click", function(event){
+    
+        event.preventDefault();
+        const recipeNameEl = $("#recipeName");
+        const mealPicEl = $("#mealPic");
+        const recipeSiteEl = $("#recipeSite");
+        const foodContainerEl=$("#food-container");
+        const foodModal=$('#food-modal');
+        let queryURL = "https://www.themealdb.com/api/json/v1/1/random.php"
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            recipeNameEl.html('<a href="' + response.meals[0].strYoutube + '" target=\*"_blank\*">' + response.meals[0].strMeal + '</a>');
+            foodContainerEl.append(recipeNameEl);
+            mealPicEl.html('<img width=\"200\" height=\"200\" src="' + response.meals[0].strMealThumb + '">');
+            foodContainerEl.append(mealPicEl);
+            recipeSiteEl.html('<a href="' + response.meals[0].strSource + '" target=\*"_blank\*>Recipe Here!</a>');
+            foodContainerEl.append(recipeSiteEl);
+            foodModal.addClass("is-active"); 
+        })
     })
-}
+    $("#close-modal").click(function() {
+        $("#cocktail-modal").removeClass("is-active");
+      });
+});
